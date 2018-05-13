57e06ad576feac90ebd89313728e7978a7a44ecd
ojplg <lees@ripco.com>
2018-05-13 17:04:28 -0500

    Parsing of names

---
+++ src/main/java/org/center4racialjustice/legup/illinois/Name.java
@@ -0,0 +1,82 @@
+package org.center4racialjustice.legup.illinois;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Name {
+
+    public static String simpleLastNameRegex = "([A-Z][a-z]+)";
+    public static String firstInitialRegex = "([A-Z][a-z]+), ([A-Z])\\.";
+    public static String fullNameRegex = "([A-Z][a-z]+), ([A-Z][a-z]+)\\s?([A-Z])?";
+    public static String fullNameWithSuffixRegex = "([A-Z][a-z]+) ([A-Z][a-z])\\., ([A-Z][a-z]+)\\s?([A-Z])?";
+
+    public static Pattern simpleLastNamePattern = Pattern.compile(simpleLastNameRegex);
+    public static Pattern firstInitialPattern = Pattern.compile(firstInitialRegex);
+    public static Pattern fullNamePattern = Pattern.compile(fullNameRegex);
+    public static Pattern fullNameWithSuffixPattern = Pattern.compile(fullNameWithSuffixRegex);
+
+    private final String firstName;
+    private final String firstInitial;
+    private final String lastName;
+    private final String middleInitial;
+    private final String suffix;
+
+    public static Name fromAnyString(String input){
+        Matcher simpleLastNameMatcher = simpleLastNamePattern.matcher(input);
+        if( simpleLastNameMatcher.matches() ){
+            return new Name(null, null, input, null, null);
+        }
+        Matcher firstInitialMatcher = firstInitialPattern.matcher(input);
+        if ( firstInitialMatcher.matches() ){
+            String lastName = firstInitialMatcher.group(1);
+            String firstInitial = firstInitialMatcher.group(2);
+            return new Name(null, null, lastName, firstInitial, null);
+        }
+        Matcher fullNameMatcher = fullNamePattern.matcher(input);
+        if( fullNameMatcher.matches() ){
+            String lastName = fullNameMatcher.group(1);
+            String firstName = fullNameMatcher.group(2);
+            String middleInitial = fullNameMatcher.group(3);
+            return new Name(firstName, middleInitial, lastName, null, null);
+        }
+
+        Matcher fullNameWithSuffixMatcher = fullNameWithSuffixPattern.matcher(input);
+        if( fullNameWithSuffixMatcher.matches() ){
+            String lastName = fullNameWithSuffixMatcher.group(1);
+            String suffix = fullNameWithSuffixMatcher.group(2);
+            String firstName = fullNameWithSuffixMatcher.group(3);
+            String middleInitial = fullNameWithSuffixMatcher.group(4);
+            return new Name(firstName, middleInitial, lastName, null, suffix);
+        }
+
+        throw new RuntimeException("Could not figure out this name: '" + input + "'");
+    }
+
+    public Name(String firstName, String middleInitial, String lastName, String firstInitial, String suffix){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.firstInitial = firstInitial;
+        this.suffix = suffix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getFirstInitial() {
+        return firstInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+}
---
+++ src/test/java/org/center4racialjustice/legup/illinois/TestName.java
@@ -0,0 +1,53 @@
+package org.center4racialjustice.legup.illinois;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestName {
+
+    @Test
+    public void justLastName(){
+        Name name = Name.fromAnyString("Jones");
+        Assert.assertEquals("Jones", name.getLastName());
+        Assert.assertNull(name.getFirstInitial());
+        Assert.assertNull(name.getFirstName());
+        Assert.assertNull(name.getMiddleInitial());
+    }
+
+    @Test
+    public void lastNameWithFirstInitial() {
+        Name name = Name.fromAnyString("Brown, E.");
+        Assert.assertEquals("Brown", name.getLastName());
+        Assert.assertEquals("E", name.getFirstInitial());
+        Assert.assertNull(name.getFirstName());
+        Assert.assertNull(name.getMiddleInitial());
+    }
+
+    @Test
+    public void fullName() {
+        Name name = Name.fromAnyString("Barickman, Jason A");
+        Assert.assertEquals("Barickman", name.getLastName());
+        Assert.assertNull(name.getFirstInitial());
+        Assert.assertEquals("Jason", name.getFirstName());
+        Assert.assertEquals("A", name.getMiddleInitial());
+    }
+
+    @Test
+    public void firstAndLastName(){
+        Name name = Name.fromAnyString("Nybo, Chris");
+        Assert.assertEquals("Nybo", name.getLastName());
+        Assert.assertNull(name.getFirstInitial());
+        Assert.assertEquals("Chris", name.getFirstName());
+        Assert.assertNull(name.getMiddleInitial());
+    }
+
+    @Test
+    public void withSuffix(){
+        Name name = Name.fromAnyString( "Clayborne Jr., James F");
+        Assert.assertEquals("Clayborne", name.getLastName());
+        Assert.assertNull(name.getFirstInitial());
+        Assert.assertEquals("James", name.getFirstName());
+        Assert.assertEquals("F", name.getMiddleInitial());
+        Assert.assertEquals("Jr", name.getSuffix());
+    }
+}
