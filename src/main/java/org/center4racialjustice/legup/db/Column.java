fac6795133267ec6d74238ae04ab7845b67e4ebf
ojplg <lees@ripco.com>
2018-08-14 15:57:47 -0500

    Intellij clean-ups

--- src/main/java/org/center4racialjustice/legup/db/Column.java
+++ src/main/java/org/center4racialjustice/legup/db/Column.java
@@ -1,36 +1,35 @@
 package org.center4racialjustice.legup.db;
 
-import java.lang.reflect.Type;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
-public class Column<O, T> {
+class Column<O, T> {
 
     private final String name;
     private final ColumnType columnType;
     private final Function<O,T> getter;
     private final BiConsumer<O,T> setter;
 
-    public Column(String name, ColumnType columnType, Function<O, T> getter, BiConsumer<O,T> setter){
+    Column(String name, ColumnType columnType, Function<O, T> getter, BiConsumer<O,T> setter){
         this.name = name;
         this.columnType = columnType;
         this.getter = getter;
         this.setter = setter;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public Function<O, T> getGetter() {
+    Function<O, T> getGetter() {
         return getter;
     }
 
-    public BiConsumer<O, T> getSetter() {
+    BiConsumer<O, T> getSetter() {
         return setter;
     }
 
-    public ColumnType getColumnType(){
+    ColumnType getColumnType(){
         return columnType;
     }
 }
