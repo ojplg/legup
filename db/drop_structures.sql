716c970ca7c3ac4cf805953d62ead4a0907b6b6c
ojplg <lees@ripco.com>
2018-08-14 09:48:29 -0500

    Drop commands

---
+++ db/drop_structures.sql
@@ -0,0 +1,17 @@
+
+begin;
+
+drop sequence person_seq;
+drop table persons;
+
+drop sequence legislator_seq;
+drop table legislators;
+
+drop sequence bill_seq;
+drop table bills;
+
+drop sequence vote_seq;
+drop table votes;
+
+end;
+
