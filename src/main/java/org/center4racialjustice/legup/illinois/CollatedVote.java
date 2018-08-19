c06e4ee1075cfcc6232d4145694b910e3ade57ce
ojplg <lees@ripco.com>
2018-08-19 16:37:21 -0500

    Dead code

--- src/main/java/org/center4racialjustice/legup/illinois/CollatedVote.java
+++ src/main/java/org/center4racialjustice/legup/illinois/CollatedVote.java
@@ -1,9 +1,10 @@
 package org.center4racialjustice.legup.illinois;
 
 import lombok.Data;
+import org.center4racialjustice.legup.domain.BetterVote;
+import org.center4racialjustice.legup.domain.Bill;
 import org.center4racialjustice.legup.domain.Legislator;
 import org.center4racialjustice.legup.domain.Name;
-import org.center4racialjustice.legup.domain.Vote;
 import org.center4racialjustice.legup.domain.VoteSide;
 
 @Data
@@ -12,11 +13,11 @@ public class CollatedVote {
     private final Legislator legislator;
     private final Name name;
 
-    public Vote asVote(long billId){
-        Vote vote = new Vote();
-        vote.setBillId(billId);
-        vote.setLegislatorId(legislator.getId());
-        vote.setVoteSide(voteSide.getCode());
+    public BetterVote asVote(Bill bill){
+        BetterVote vote = new BetterVote();
+        vote.setBill(bill);
+        vote.setLegislator(legislator);
+        vote.setVoteSide(voteSide);
         return vote;
     }
 }
