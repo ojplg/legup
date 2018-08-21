fa9c547b6175bb227b1826a565f6e36ea6428138
ojplg <lees@ripco.com>
2018-08-21 16:22:05 -0500

    Start persistence of report cards

---
+++ src/main/java/org/center4racialjustice/legup/db/ReportCardDao.java
@@ -0,0 +1,27 @@
+package org.center4racialjustice.legup.db;
+
+import org.center4racialjustice.legup.domain.ReportCard;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ReportCardDao {
+    public static String table = "REPORT_CARDS";
+
+    public static List<TypedColumn<ReportCard>> typedColumnList =
+            Arrays.asList(
+                    new LongColumn<>("ID", "", ReportCard::getId, ReportCard::setId),
+                    new StringColumn<>("NAME", "", ReportCard::getName, ReportCard::setName),
+                    new LongColumn<>("SESSION_NUMBER", "", ReportCard::getSessionNumber, ReportCard::setSessionNumber)
+            );
+
+    public static Supplier<ReportCard> supplier = () -> new ReportCard();
+
+    private final Connection connection;
+
+    public ReportCardDao(Connection connection) {
+        this.connection = connection;
+    }
+}
---
+++ src/main/java/org/center4racialjustice/legup/db/ReportFactorDao.java
@@ -0,0 +1,37 @@
+package org.center4racialjustice.legup.db;
+
+import org.center4racialjustice.legup.domain.Bill;
+import org.center4racialjustice.legup.domain.ReportCard;
+import org.center4racialjustice.legup.domain.ReportFactor;
+import org.center4racialjustice.legup.domain.Vote;
+import org.center4racialjustice.legup.domain.VoteSideConverter;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ReportFactorDao {
+    public static String table = "REPORT_FACTORS";
+
+    public static List<TypedColumn<ReportFactor>> dataColumns =
+            Arrays.asList(
+                    new LongColumn<>("ID", "", ReportFactor::getId, ReportFactor::setId),
+                    new CodedEnumColumn<>("VOTE_SIDE", "", ReportFactor::getVoteSide, ReportFactor::setVoteSide, VoteSideConverter.INSTANCE)
+            );
+
+    private static final JoinColumn<ReportFactor,Bill> billColumn =
+            new JoinColumn<>("BILL_ID", "b", "bills", ReportFactor::getBill, ReportFactor::setBill,
+                    BillDao.supplier, BillDao.typedColumnList );
+
+    public static List<JoinColumn<ReportFactor, ?>> joinColumns =
+            Arrays.asList( billColumn );
+
+    public static Supplier<ReportFactor> supplier = () -> new ReportFactor();
+
+    private final Connection connection;
+
+    public ReportFactorDao(Connection connection) {
+        this.connection = connection;
+    }
+}
