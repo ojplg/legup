8da412d82a170e47d6f9c4a76d6e796b10f1ba7d
ojplg <lees@ripco.com>
2018-08-23 12:01:26 -0500

    Bill searching improvement

--- src/main/java/org/center4racialjustice/legup/web/handlers/ViewBillSearchResults.java
+++ src/main/java/org/center4racialjustice/legup/web/handlers/ViewBillSearchResults.java
@@ -1,6 +1,8 @@
 package org.center4racialjustice.legup.web.handlers;
 
 import org.apache.velocity.VelocityContext;
+import org.center4racialjustice.legup.domain.Chamber;
+import org.center4racialjustice.legup.illinois.BillSearcher;
 import org.center4racialjustice.legup.web.Handler;
 import org.center4racialjustice.legup.web.Util;
 import org.eclipse.jetty.server.Request;
@@ -15,13 +17,21 @@ public class ViewBillSearchResults implements Handler {
     public VelocityContext handle(Request request, HttpServletResponse httpServletResponse) throws IOException, SQLException {
 
         String chamberString = request.getParameter("chamber");
+        Chamber chamber = Chamber.fromString(chamberString);
         Long number = Util.getLongParameter(request, "number");
 
+        BillSearcher searcher = new BillSearcher();
+
+        String billHomePageUrl = searcher.searchForBaseUrl(chamber, number);
+        String votesUrl = searcher.convertToVotesPage(billHomePageUrl);
 
         VelocityContext velocityContext = new VelocityContext();
 
         velocityContext.put("chamber", chamberString);
         velocityContext.put("number", number);
+
+        velocityContext.put("bill_home_page_url", billHomePageUrl);
+        velocityContext.put("bill_vote_page_url", votesUrl);
 
         return velocityContext;
     }
