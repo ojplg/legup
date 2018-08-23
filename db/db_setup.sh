235682b0ca13ee91fbdeb1d518cc5a8474747d6e
ojplg <lees@ripco.com>
2018-08-23 10:00:49 -0500

    New scripts for creating structues and db users

---
+++ db/db_setup.sh
@@ -0,0 +1,8 @@
+sudo -u postgres psql -f setup.sql
+sudo -u postgres psql -f create_dev_db.sql
+sudo -u postgres psql -f create_test_db.sql
+sudo -u postgres psql -f create_db.sql
+psql -f create_structures.sql legup_test
+psql -f create_structures.sql legup_dev
+psql -f create_structures.sql legup
+
