4086e0ad863525437dad8e9ff54a572bdf16ff18
ojplg <lees@ripco.com>
2018-08-23 19:31:28 -0500

    Improve sponsor parsing

--- src/main/java/org/center4racialjustice/legup/illinois/BillHtmlParser.java
+++ src/main/java/org/center4racialjustice/legup/illinois/BillHtmlParser.java
@@ -41,28 +41,61 @@ public class BillHtmlParser {
     }
 
     public List<String> getSponsorNames(Chamber chamber){
-        Elements anchors = document.select("a").attr("class", "content notranslate");
+        List<String> names = new ArrayList<>();
 
         String urlRegex;
+        String startSpanText;
         switch (chamber.toString()) {
             case "House" :
+                startSpanText = "House Sponsors";
                 urlRegex = ".*house/rep\\.asp.*";
                 break;
             case "Senate":
+                startSpanText = "Senate Sponsors";
                 urlRegex = ".*senate/Senator\\.asp.*";
                 break;
             default :
                 throw new RuntimeException("Unknown chamber " + chamber);
         }
 
-        List<String> names = new ArrayList<>();
-        for (Element anchor : anchors){
-            String href = anchor.attr("href");
-            if ( href.matches(urlRegex)){
-                String txt = anchor.text();
-                names.add(txt.replace("Rep. ",""));
+        boolean started = false;
+        boolean found = false;
+        int cnt = 0;
+        int nameCnt = 0;
+        for ( Element element : document.getAllElements() ){
+            cnt++;
+            String nodeName = element.nodeName();
+            if ("span".equals(nodeName)){
+                String text = element.text();
+                if ( text.equals(startSpanText)){
+                    started = true;
+                }
+            }
+            if( started && "a".equals(nodeName)){
+                nameCnt ++;
+                found = true;
+                String text = element.text();
+                names.add(text.replace("Rep. ", ""));
+            }
+            if (started && found && "br".equals(nodeName)){
+                break;
             }
         }
+
+//        Elements anchors = document.select("a").attr("class", "content notranslate");
+//        boolean started = false;
+//        for (Element anchor : anchors){
+//            String href = anchor.attr("href");
+//            if ( href.matches(urlRegex)){
+//                started = true;
+//                String txt = anchor.text();
+//                names.add(txt.replace("Rep. ",""));
+//            } else {
+//                if (started){
+//                    break;
+//                }
+//            }
+//        }
         return names;
     }
 }
--- src/main/java/org/center4racialjustice/legup/web/handlers/ViewBillSearchResults.java
+++ src/main/java/org/center4racialjustice/legup/web/handlers/ViewBillSearchResults.java
@@ -1,7 +1,9 @@
 package org.center4racialjustice.legup.web.handlers;
 
 import org.apache.velocity.VelocityContext;
+import org.center4racialjustice.legup.domain.Bill;
 import org.center4racialjustice.legup.domain.Chamber;
+import org.center4racialjustice.legup.illinois.BillHtmlParser;
 import org.center4racialjustice.legup.illinois.BillSearcher;
 import org.center4racialjustice.legup.web.Handler;
 import org.center4racialjustice.legup.web.Util;
@@ -26,16 +28,25 @@ public class ViewBillSearchResults implements Handler {
         String billHomePageUrl = searcher.searchForBaseUrl(chamber, number);
         String votesUrl = searcher.convertToVotesPage(billHomePageUrl);
 
+        BillHtmlParser billHtmlParser = new BillHtmlParser(billHomePageUrl);
+        Bill bill = new Bill();
+        bill.setChamber(chamber);
+        bill.setNumber(number);
+        // FIXME!!!
+        bill.setSession(100);
+        bill.setShortDescription(billHtmlParser.getShortDescription());
+
         Map<String, String> votesUrlsMap = searcher.searchForVotesUrls(votesUrl);
 
         VelocityContext velocityContext = new VelocityContext();
 
-        velocityContext.put("chamber", chamberString);
-        velocityContext.put("number", number);
+        velocityContext.put("bill", bill);
 
         velocityContext.put("bill_home_page_url", billHomePageUrl);
         velocityContext.put("bill_vote_page_url", votesUrl);
         velocityContext.put("votes_url_map", votesUrlsMap);
+        velocityContext.put("house_sponsors", billHtmlParser.getSponsorNames(Chamber.House));
+        velocityContext.put("senate_sponsors", billHtmlParser.getSponsorNames(Chamber.Senate));
 
         return velocityContext;
     }
