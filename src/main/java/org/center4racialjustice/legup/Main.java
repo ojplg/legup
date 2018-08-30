5f9bd692446953602f5ed5cc78167d4c9ad006da
ojplg <lees@ripco.com>
2018-08-30 11:03:11 -0500

    Start up improved

--- src/main/java/org/center4racialjustice/legup/Main.java
+++ src/main/java/org/center4racialjustice/legup/Main.java
@@ -1,25 +1,15 @@
 package org.center4racialjustice.legup;
 
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.velocity.app.Velocity;
-import org.center4racialjustice.legup.db.BillMapper;
 import org.center4racialjustice.legup.db.ConnectionPool;
-import org.center4racialjustice.legup.domain.Bill;
-import org.center4racialjustice.legup.domain.Chamber;
+import org.center4racialjustice.legup.db.MybatisStarter;
 import org.center4racialjustice.legup.web.ServerStarter;
-import org.postgresql.ds.PGSimpleDataSource;
 
 import java.io.FileReader;
 import java.io.Reader;
-import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -38,6 +28,7 @@ public class Main {
                 properties.load(reader);
             } else {
                 log.info("Using default properties");
+                properties.setProperty("db.name", "legup");
                 properties.setProperty("db.url","jdbc:postgresql://localhost:5432/legup");
                 properties.setProperty("db.user","legupuser");
                 properties.setProperty("db.password","legupuserpass");
@@ -46,16 +37,19 @@ public class Main {
             // make sure postgres drivers are loaded
             Class.forName("org.postgresql.Driver");
 
-
             ConnectionPool pool = new ConnectionPool(
                     properties.getProperty("db.url"),
                     properties.getProperty("db.user"),
                     properties.getProperty("db.password")
             );
 
-            SqlSessionFactory sqlSessionFactory = configureIbatis();
-            testIbatis(sqlSessionFactory);
+            MybatisStarter mybatisStarter = new MybatisStarter(
+                    properties.getProperty("db.name"),
+                    properties.getProperty("db.user"),
+                    properties.getProperty("db.password")
+            );
 
+            SqlSessionFactory sqlSessionFactory = mybatisStarter.sessionFactory();
             pool.setSqlSessionFactory(sqlSessionFactory);
 
             Properties velocityProperties = new Properties();
@@ -70,65 +64,4 @@ public class Main {
             ex.printStackTrace();
         }
     }
-
-    private static void testIbatis(SqlSessionFactory sqlSessionFactory){
-
-        try(SqlSession sqlSession = sqlSessionFactory.openSession()){
-            BillMapper mapper = sqlSession.getMapper(BillMapper.class);
-
-            Bill bill = mapper.selectBill(26);
-
-            log.info("Found bill 26 !! " + bill);
-
-//            Bill newBill = new Bill();
-//            newBill.setShortDescription("Puff");
-//            newBill.setSession(99);
-//            newBill.setNumber(System.currentTimeMillis() % 1000000);
-//            newBill.setChamber(Chamber.House);
-//
-//            mapper.insert(newBill);
-
-            List<Bill> allBills = mapper.selectBills();
-            for(Bill b : allBills){
-                log.info("******** " + b);
-            }
-
-            log.info("---");
-            log.info("---");
-            log.info("---");
-            log.info("---");
-
-            Bill bill2771 = mapper.selectBillBySessionChamberAndNumber(100,Chamber.House, 2771);
-
-            log.info("#########################" + bill2771);
-
-            sqlSession.commit();
-        }
-
-    }
-
-    private static SqlSessionFactory configureIbatis() {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setDatabaseName("legup");
-        dataSource.setUser("legupuser");
-        dataSource.setPassword("legupuserpass");
-        // Might need server name and port (localhost and default)
-
-        TransactionFactory transactionFactory =
-                new JdbcTransactionFactory();
-        Environment environment =
-                new Environment("development", transactionFactory, dataSource);
-        Configuration configuration = new Configuration(environment);
-        configuration.addMapper(BillMapper.class);
-
-        SqlSessionFactory sqlSessionFactory =
-                new SqlSessionFactoryBuilder().build(configuration);
-
-        log.info("**IBATIS** Configuration built " + configuration);
-        log.info("**IBATIS** SQLSession Factory built " + sqlSessionFactory);
-
-        return sqlSessionFactory;
-    }
-
-
 }
