019c79a2fa7579fa418f4f453a49ddd20ccd38e5
ojplg <lees@ripco.com>
2018-09-12 16:30:41 -0500

    Import fix

--- src/main/java/org/center4racialjustice/legup/illinois/MemberHtmlParser.java
+++ src/main/java/org/center4racialjustice/legup/illinois/MemberHtmlParser.java
@@ -9,7 +9,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
