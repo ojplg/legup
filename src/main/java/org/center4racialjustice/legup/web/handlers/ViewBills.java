b112b92da4dbdb946838386552634aa1aecd7c02
ojplg <lees@ripco.com>
2018-09-14 17:50:34 -0500

    Navigation

--- src/main/java/org/center4racialjustice/legup/web/handlers/ViewBills.java
+++ src/main/java/org/center4racialjustice/legup/web/handlers/ViewBills.java
@@ -5,6 +5,7 @@ import org.center4racialjustice.legup.db.BillDao;
 import org.center4racialjustice.legup.db.ConnectionPool;
 import org.center4racialjustice.legup.db.ConnectionWrapper;
 import org.center4racialjustice.legup.domain.Bill;
+import org.center4racialjustice.legup.domain.Chamber;
 import org.center4racialjustice.legup.util.Tuple;
 import org.center4racialjustice.legup.web.Handler;
 import org.center4racialjustice.legup.web.LegupSession;
@@ -25,14 +26,17 @@ public class ViewBills implements Handler {
     public VelocityContext handle(Request request, LegupSession legupSession, HttpServletResponse httpServletResponse) {
 
         try (ConnectionWrapper connection = connectionPool.getWrappedConnection() ){
-            BillDao dao = new BillDao(connection);
 
+            BillDao dao = new BillDao(connection);
             List<Bill> bills = dao.readAll();
-            Tuple<List<Bill>, List<Bill>> divideBills = Bill.divideAndOrder(bills);
+
+            Tuple<List<Bill>, List<Bill>> dividedBills = Bill.divideAndOrder(bills);
 
             VelocityContext velocityContext = new VelocityContext();
-            velocityContext.put("house_bills", divideBills.getFirst());
-            velocityContext.put("senate_bills", divideBills.getSecond());
+            velocityContext.put("house", Chamber.House);
+            velocityContext.put("senate", Chamber.Senate);
+            velocityContext.put("house_bills", dividedBills.getFirst());
+            velocityContext.put("senate_bills", dividedBills.getSecond());
             return velocityContext;
         }
     }
