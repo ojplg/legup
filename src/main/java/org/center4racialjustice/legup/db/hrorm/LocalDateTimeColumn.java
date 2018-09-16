ed6ba58b80f1fa328bcdcc0c4d2cef6e0d6c8018
ojplg <lees@ripco.com>
2018-09-16 13:08:10 -0500

    simpler

--- src/main/java/org/center4racialjustice/legup/db/hrorm/LocalDateTimeColumn.java
+++ src/main/java/org/center4racialjustice/legup/db/hrorm/LocalDateTimeColumn.java
@@ -5,8 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
@@ -16,15 +14,12 @@ public class LocalDateTimeColumn<T> implements TypedColumn<T> {
     private final String prefix;
     private final BiConsumer<T, LocalDateTime> setter;
     private final Function<T, LocalDateTime> getter;
-    private final ZoneId zoneId;
 
     public LocalDateTimeColumn(String name, String prefix, Function<T, LocalDateTime> getter, BiConsumer<T, LocalDateTime> setter) {
         this.name = name;
         this.prefix = prefix;
         this.getter = getter;
         this.setter = setter;
-        // FIXME: Needs to be settable and default to UTC
-        this.zoneId = ZoneId.of("America/Chicago");
     }
 
     @Override
@@ -52,8 +47,7 @@ public class LocalDateTimeColumn<T> implements TypedColumn<T> {
     @Override
     public void setValue(T item, int index, PreparedStatement preparedStatement) throws SQLException {
         LocalDateTime value = getter.apply(item);
-        ZoneOffset zoneOffset = zoneId.getRules().getOffset(value);
-        Timestamp sqlTime = Timestamp.from(value.toInstant(zoneOffset));
+        Timestamp sqlTime = Timestamp.valueOf(value);
         preparedStatement.setTimestamp(index, sqlTime);
     }
 }
