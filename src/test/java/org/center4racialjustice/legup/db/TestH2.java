6bcf4999d4a9086d6e892f1a9b1ced7d1b4b2a17
ojplg <lees@ripco.com>
2018-09-18 14:49:16 -0500

    Start to get H2 working

--- src/test/java/org/center4racialjustice/legup/db/TestH2.java
+++ src/test/java/org/center4racialjustice/legup/db/TestH2.java
@@ -3,54 +3,103 @@ package org.center4racialjustice.legup.db;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class TestH2 {
 
+    private static boolean initialized = false;
+    private static String connectionUrl = "jdbc:h2:./db/legup_h2_test";
+
     public static Connection getConnection(){
         try {
-
-            Class.forName("org.h2.Driver" );
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/legup");
-            return connection;
-        } catch (ClassNotFoundException | SQLException ex) {
+            if ( ! initialized ) {
+                cleanUpOldDb();
+                initializeDb();
+                initialized = true;
+            }
+            return DriverManager.getConnection(connectionUrl);
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
 
+    private static void cleanUpOldDb(){
+        try {
+            Path path = Paths.get("./db/legup_h2_test.mv.db");
+            Files.deleteIfExists(path);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void initializeDb(){
+        try {
+            Path path = Paths.get("./db/create_h2_structures.sql");
+            List<String> lines = Files.readAllLines(path);
+
+            StringBuilder wholeFileBuffer = new StringBuilder();
+            for( String line : lines){
+                if(line.startsWith("begin;")
+                        || line.startsWith("grant all")
+                        || line.startsWith("end;")
+                        || line.matches("alter table \\w+ add constraint \\w+ unique.*")){
+                    continue;
+                }
+                wholeFileBuffer.append(line);
+                wholeFileBuffer.append("\n");
+            }
+            String wholeFile = wholeFileBuffer.toString();
+
+            Class.forName("org.h2.Driver" );
+            Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();
+            statement.execute(wholeFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 
     @Test
-    public void createStuff(){
+    public void testPullFromSequence(){
 
         try {
-            String createSequence = "create sequence if not exists legislator_seq";
+            String selectSql = "select nextval('legislator_seq')";
+
             Connection connection = getConnection();
-
             Statement statement = connection.createStatement();
-            boolean created = statement.execute(createSequence);
 
-            Assert.assertFalse(created);
-
-            statement.close();
-
-            statement = connection.createStatement();
-
-            String selectValue = "select nextval('legislator_seq')";
-
-            boolean selected = statement.execute(selectValue);
-
+            boolean selected = statement.execute(selectSql);
             Assert.assertTrue(selected);
 
             ResultSet resultSet = statement.getResultSet();
             resultSet.next();
-            long foo = resultSet.getLong(1);
+            long pull1 = resultSet.getLong(1);
 
-            Assert.assertTrue(foo > 0);
+            Assert.assertTrue(pull1 > 0);
 
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+            connection = getConnection();
+            statement = connection.createStatement();
+
+            statement.execute(selectSql);
+
+            resultSet = statement.getResultSet();
+            resultSet.next();
+            long pull2 = resultSet.getLong(1);
+
+            Assert.assertTrue(pull2 > pull1);
         } catch (Exception ex){
             ex.printStackTrace();
             throw new RuntimeException(ex);
