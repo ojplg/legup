193ed3f88e3ad6bacfb9019b0a2319f86209e430
ojplg <lees@ripco.com>
2018-09-27 09:59:09 -0500

    Some comments

--- src/main/java/org/center4racialjustice/legup/web/LegupResponse.java
+++ src/main/java/org/center4racialjustice/legup/web/LegupResponse.java
@@ -4,6 +4,10 @@ import org.apache.velocity.VelocityContext;
 
 public class LegupResponse {
 
+    // 1 template name
+    // 2 use container
+    // 3 content type
+
     private final VelocityContext velocityContext;
 
     public LegupResponse() {
