87d59eaaa067308edbd8a0bd219fcdb49372cf65
ojplg <lees@ripco.com>
2018-09-28 15:32:48 -0500

    clean up

--- src/main/java/org/center4racialjustice/legup/db/hrorm/SqlBuilder.java
+++ src/main/java/org/center4racialjustice/legup/db/hrorm/SqlBuilder.java
@@ -1,7 +1,5 @@
 package org.center4racialjustice.legup.db.hrorm;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
