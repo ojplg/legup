8aa428c3a54bb7a9db64014cfa53ddbc64eb3f2a
ojplg <lees@ripco.com>
2018-09-29 11:08:57 -0500

    Script for making backups supports aws

--- script/backup_db.sh
+++ script/backup_db.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
 # Script for taking a back-up of the legup database
 # Use psql to restore
+
+if [ "$1" = aws ]; 
+  then
+    SUDO="sudo postgres"
+fi
 
 DATE=`date +%Y%m%d`
 FILEPATH=/var/legup/db_dumps/$DATE.sql
 
 echo "Making legup backup for $DATE to $FILEPATH"
 
-pg_dump legup > $FILEPATH
+$SUDO pg_dump legup > $FILEPATH
