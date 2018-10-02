13875f64b3a65b9babde23cad6e0762d638d009b
ojplg <lees@ripco.com>
2018-10-01 21:16:03 -0500

    Better stuffings

--- src/test/java/org/center4racialjustice/legup/db/DbTestConfigs.java
+++ src/test/java/org/center4racialjustice/legup/db/DbTestConfigs.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 
 public class DbTestConfigs {
 
+    public static final String PostgresUrl = "jdbc:postgresql://localhost:5432/legup_test";
+    public static final String PostgresUser = "legupuser";
+    public static final String PostgresPassword = "legupuserpass";
+
     private static final H2ConnectionFactory h2ConnectionFactory = new H2ConnectionFactory();
     
     private static final PostgresConnectionFactory postgresConnectionFactory = new PostgresConnectionFactory(
-            "jdbc:postgresql://localhost:5432/legup_test","legupuser", "legupuserpass");
+            PostgresUrl,PostgresUser, PostgresPassword);
 
     public static Connection connect(){
 //        return getPostgresConnection();
