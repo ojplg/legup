7ce9a7e70b683a5eafb0708272677f086a84ac10
ojplg <lees@ripco.com>
2018-11-02 19:08:18 -0500

    Work on passwords

--- src/main/java/org/center4racialjustice/legup/domain/User.java
+++ src/main/java/org/center4racialjustice/legup/domain/User.java
@@ -2,6 +2,14 @@ package org.center4racialjustice.legup.domain;
 
 import lombok.Data;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
 @Data
 public class User {
     private Long id;
@@ -9,4 +17,35 @@ public class User {
     private String password;
     private String salt;
     private Organization organization;
+
+    private static String encrypt(String password, String salt) {
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            KeySpec ks = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 1024, 128);
+            SecretKey s = f.generateSecret(ks);
+            return new String(s.getEncoded());
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static User createNewUser(String email, String unencryptedPassword){
+        User user = new User();
+        user.salt = newSalt();
+        user.email = email;
+        user.password = encrypt(unencryptedPassword, user.salt);
+        return user;
+    }
+
+    public boolean correctPassword(String unencryptedPassword){
+        String encryptedPassword = encrypt(unencryptedPassword, this.salt);
+        return encryptedPassword.equals(this.password);
+    }
+
+    public static String newSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+        return new String(bytes);
+    }
 }
---
+++ src/test/java/org/center4racialjustice/legup/domain/TestUser.java
@@ -0,0 +1,58 @@
+package org.center4racialjustice.legup.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestUser {
+
+    @Test
+    public void testSaltGeneration(){
+        String salt1 = User.newSalt();
+        String salt2 = User.newSalt();
+
+        Assert.assertNotNull(salt1);
+        Assert.assertNotNull(salt2);
+
+        Assert.assertNotEquals(salt1, salt2);
+
+        Assert.assertTrue( salt1.length() > 20);
+        Assert.assertTrue( salt2.length() > 20);
+    }
+
+    @Test
+    public void testCreateNewUser(){
+
+        String unencryptedPassword = "p@ssw0rd1!";
+
+        User user = User.createNewUser("foo@bar.com", unencryptedPassword);
+
+        Assert.assertNotNull(user.getSalt());
+        Assert.assertTrue(user.getSalt().length() > 20);
+
+        Assert.assertNotNull(user.getPassword());
+        Assert.assertNotEquals(unencryptedPassword, user.getPassword());
+
+        Assert.assertTrue(user.getPassword().length() > 10);
+    }
+
+    @Test
+    public void testMatchesPassword(){
+
+        String unencryptedPassword = "p@ssw0rd1!";
+
+        User user = User.createNewUser("foo@bar.com", unencryptedPassword);
+
+        Assert.assertTrue(user.correctPassword(unencryptedPassword));
+    }
+
+    @Test
+    public void testMatchesPasswordNoticesWrongPasswords(){
+
+        String unencryptedPassword = "p@ssw0rd1!";
+
+        User user = User.createNewUser("foo@bar.com", unencryptedPassword);
+
+        Assert.assertFalse(user.correctPassword("password"));
+    }
+
+}
