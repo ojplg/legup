2b5b4c90c2ce5ef74a17987c8debf5a6ba454543
ojplg <lees@ripco.com>
2018-11-09 18:30:29 -0600

    Last navbar conversion

--- src/main/java/org/center4racialjustice/legup/web/responders/ViewReportCardLegislator.java
+++ src/main/java/org/center4racialjustice/legup/web/responders/ViewReportCardLegislator.java
@@ -7,9 +7,12 @@ import org.center4racialjustice.legup.web.HtmlLegupResponse;
 import org.center4racialjustice.legup.web.LegupResponse;
 import org.center4racialjustice.legup.web.LegupSession;
 import org.center4racialjustice.legup.web.LegupSubmission;
+import org.center4racialjustice.legup.web.NavLink;
 import org.center4racialjustice.legup.web.Responder;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class ViewReportCardLegislator implements Responder {
 
@@ -22,7 +25,8 @@ public class ViewReportCardLegislator implements Responder {
         ReportCardGrades reportCardGrades = (ReportCardGrades) submission.getObject(LegupSession.ReportCardGradesKey);
         ReportCardLegislatorAnalysis reportCardLegislatorAnalysis = reportCardGrades.getLegislatorAnalysis(legislatorId);
 
-        HtmlLegupResponse response = new HtmlLegupResponse(this.getClass());
+        HtmlLegupResponse response = HtmlLegupResponse.withLinks(this.getClass(),
+                submission.getLoggedInUser(), navLinks(reportCardGrades.getReportCard().getId()));
 
         response.putVelocityData("oneTimeKey", oneTimeKey);
         response.putVelocityData("reportCardGrades", reportCardGrades);
@@ -36,4 +40,11 @@ public class ViewReportCardLegislator implements Responder {
 
         return response;
     }
+
+    private List<NavLink> navLinks(long reportCardId){
+        return Arrays.asList(
+                new NavLink("All Scores", "/legup/view_report_card_scores?report_card_id=" + reportCardId)
+        );
+    }
+
 }
