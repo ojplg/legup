d8f629cb4f42ee2da8ab7a8d4ec545fd1a0a3d7a
ojplg <lees@ripco.com>
2019-06-20 18:21:40 -0500

    A little javascript to make bulk selection easier

--- src/main/resources/js/legup.js
+++ src/main/resources/js/legup.js
@@ -3,6 +3,20 @@ var Legup = {
     helpWindow : function(url){
         console.log("Opening window to " + url);
         window.open(url, "", "height=500px,width=400px");
+    },
+
+    setCheckedByClassName : function(className){
+         var checkBoxes = document.getElementsByClassName(className);
+         for(var i=0; i<checkBoxes.length; i++){
+            checkBoxes[i].checked = true;
+         }
+    },
+
+    setUncheckedByClassName : function(className){
+         var checkBoxes = document.getElementsByClassName(className);
+         for(var i=0; i<checkBoxes.length; i++){
+            checkBoxes[i].checked = false;
+         }
     }
 
 };
