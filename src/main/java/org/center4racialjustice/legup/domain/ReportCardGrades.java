d869deecbdd8a48c422eea39688ef2466d44d517
ojplg <lees@ripco.com>
2019-06-20 19:00:15 -0500

    Separate scoring for house and senate

--- src/main/java/org/center4racialjustice/legup/domain/ReportCardGrades.java
+++ src/main/java/org/center4racialjustice/legup/domain/ReportCardGrades.java
@@ -14,8 +14,10 @@ public class ReportCardGrades {
     private final ReportCard reportCard;
     private final List<BillAction> actions;
     private final LookupTable<Legislator, Bill, Integer> lookupTable;
-    private final Grader grader;
-    private final Map<Legislator, Grade> grades;
+    private final Grader houseGrader;
+    private final Grader senateGrader;
+    private final Map<Legislator, Grade> houseGrades;
+    private final Map<Legislator, Grade> senateGrades;
 
     public ReportCardGrades(ReportCard reportCard, List<BillAction> actions){
         this.reportCard = reportCard;
@@ -23,9 +25,14 @@ public class ReportCardGrades {
 
         this.lookupTable = reportCard.calculateScores(actions);
 
-        Map<Legislator, Integer> sums = sumScores();
-        this.grader = new Grader(reportCard.getGradeLevels(), sums.values());
-        this.grades = assignGrades(sums);
+        Map<Legislator, Integer> houseSums = sumScores(Chamber.House);
+        this.houseGrader = new Grader(reportCard.getGradeLevels(), houseSums.values());
+
+        Map<Legislator, Integer> senateSums = sumScores(Chamber.Senate);
+        this.senateGrader = new Grader(reportCard.getGradeLevels(), senateSums.values());
+
+        this.houseGrades = assignGrades(houseSums, houseGrader);
+        this.senateGrades = assignGrades(senateSums, senateGrader);
     }
 
     public ReportCard getReportCard() { return  reportCard;}
@@ -39,7 +46,10 @@ public class ReportCardGrades {
     }
 
     public Map<Legislator, Grade> getGrades(){
-        return grades;
+        Map<Legislator, Grade> allGrades = new HashMap<>();
+        allGrades.putAll(houseGrades);
+        allGrades.putAll(senateGrades);
+        return allGrades;
     }
 
     public List<Bill> getBills(){
@@ -50,7 +60,7 @@ public class ReportCardGrades {
         return lookupTable.sortedRowHeadings(Legislator::compareTo);
     }
 
-    private Map<Legislator, Grade> assignGrades(Map<Legislator, Integer> sums){
+    private Map<Legislator, Grade> assignGrades(Map<Legislator, Integer> sums, Grader grader){
         Map<Legislator, Grade> grades = new HashMap<>();
         for( Map.Entry<Legislator, Integer> entry : sums.entrySet()){
             Grade grade = grader.assignGrade(entry.getValue());
@@ -64,14 +74,14 @@ public class ReportCardGrades {
         List<BillAction> billActions = actions.stream()
                 .filter(a -> billId == a.getBill().getId())
                 .collect(Collectors.toList());
-        return new ReportCardBillAnalysis(reportFactor, billActions, grades);
+        return new ReportCardBillAnalysis(reportFactor, billActions, getGrades());
     }
 
     public ReportCardLegislatorAnalysis getLegislatorAnalysis(long legislatorId){
         Legislator legislator = Lists.findfirst(
-                new ArrayList<>(grades.keySet()),
+                new ArrayList<>(getGrades().keySet()),
                 l -> l.getId() == legislatorId );
-        Grade grade = grades.get(legislator);
+        Grade grade = getGrades().get(legislator);
         List<BillAction> legislatorActions = actions.stream()
                 .filter(a -> a.getLegislator().equals(legislator))
                 .collect(Collectors.toList());
@@ -79,27 +89,41 @@ public class ReportCardGrades {
         return new ReportCardLegislatorAnalysis(reportCard, legislator, grade, legislatorActions);
     }
 
-    private Map<Legislator, Integer> sumScores() {
+    private Map<Legislator, Integer> sumScores(Chamber chamber) {
         Map<Legislator, Integer> sums = new HashMap<>();
 
         for (Legislator legislator : lookupTable.getRowHeadings()) {
-            Integer sum = lookupTable.computeRowSummary(legislator, 0, ReportCard.ScoreComputer);
-            sums.put(legislator, sum);
+            if( legislator.getChamber().equals(chamber)) {
+                Integer sum = lookupTable.computeRowSummary(legislator, 0, ReportCard.ScoreComputer);
+                sums.put(legislator, sum);
+            }
         }
 
         return sums;
     }
 
-    public int getLowScore() {
-        return grader.getLowScore();
+    public int getLowHouseScore() {
+        return houseGrader.getLowScore();
     }
 
-    public int getHighScore() {
-        return grader.getHighScore();
+    public int getHighHouseScore() {
+        return houseGrader.getHighScore();
     }
 
-    public int getMean() {
-        return grader.getMean();
+    public int getHouseMean() {
+        return houseGrader.getMean();
+    }
+
+    public int getLowSenateScore() {
+        return senateGrader.getLowScore();
+    }
+
+    public int getHighSenateScore() {
+        return senateGrader.getHighScore();
+    }
+
+    public int getSenateMean() {
+        return senateGrader.getMean();
     }
 
 }
